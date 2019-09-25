687039889411dc5f9263442f3c0e063131c24e8e
Joshua Darnell <josh@kurotek.com>
2019-09-25 04:52:56 -0700

    adding /genEvent/:numEvents endpoint

--- api/app.js
+++ api/app.js
@@ -4,22 +4,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const {
-  initData, getEvent, getEvents, getResourceData} = require('./data');
+const NUM_EVENTS_DEFAULT = 100;
 
-app.get('/', (req, res) => res.send('Welcome to the Log Prototype App'));
+const {
+  initData, 
+  getEvent, 
+  getEvents, 
+  getResourceData,
+  RAND,
+} = require('./data');
+
+app.get('/', (req, res) => res.send('Welcome to the Log Prototype App!'));
 
 //  Just to prevent 400s in :resource
-app.get('/favicon.ico', (req, res) => {
-  res.send('');
-});
+app.get('/favicon.ico', (req, res) => res.send(''));
 
-// events endpoint
+// event and resource data endpoints
 app.get('/events', (req, res) => res.json(getEvents()));
-
 app.get('/events/gte/:entityEventSequence', (req, res) => res.json(getEvents(req.params.entityEventSequence)));
+app.get('/genEvents/:numEvents', (req, res) => {RAND.genEvents(req.params.numEvents); res.sendStatus(200);}); 
 
 app.get('/:resource/:id', (req, res) => res.json(getResourceData(req.params.resource, req.params.id)));
+
 
 // start app
 app.listen(port, () => {
--- api/data.js
+++ api/data.js
@@ -1,24 +1,102 @@
 /* Data Generation Service */
 
 const DEBUG = false;
-const DEFAULT_POOL_SIZE = 10**5;
+
+const DEFAULT_POOL_SIZE = 10**3;
 const HOST = "http://localhost:3000";
 const createRecordUrl = (resourceName, resourceKey) => `${HOST}/${resourceName}/${resourceKey}`;
 const getRandomInt = (size=Number.MAX_SAFE_INTEGER) => Math.floor(Math.random() * Math.floor(size));
+const getTimestamp = () => new Date().toISOString();
 
 const faker = require('faker');
 const _cliProgress = require('cli-progress');
 const _generatorProgressBar = new _cliProgress.SingleBar({}, _cliProgress.Presets.shades_classic);
 
-const LOG = msg => {if (DEBUG) console.log(msg)};
+const LOG = msg => {if (DEBUG) console.log(`[${getTimestamp()}]: ${msg}`);};
 
 const METADATA = {
-  OFFICE: {name: 'Office', keyField: 'officeKey', generator: 'randOffice'},
-  MEMBER: {name: 'Member', keyField: 'memberKey', generator: 'randMember'},
-  PROPERTY: {name: 'Property', keyField: 'listingKey', generator: 'randProperty'},
-  MEDIA: {name: "Media", keyField: 'mediaKey', generator: 'randMedia'},
-  OPEN_HOUSE: {name: 'OpenHouse', keyField: 'openHouseKey', generator: 'randOpenHouse'},
+  OFFICE: {name: 'Office', keyField: 'officeKey', generator: 'RAND.genOffice'},
+  MEMBER: {name: 'Member', keyField: 'memberKey', generator: 'RAND.genMember'},
+  PROPERTY: {name: 'Property', keyField: 'listingKey', generator: 'RAND.genProperty'},
+  MEDIA: {name: "Media", keyField: 'mediaKey', generator: 'RAND.genMedia'},
+  OPEN_HOUSE: {name: 'OpenHouse', keyField: 'openHouseKey', generator: 'RAND.genOpenHouse'},
 };
+
+const RAND = {
+  //TODO: use something like JSM, which adds faker to JSON Schema models.
+  genOffice () { 
+    return {
+      officeKey: getRandomInt(),
+      officeName: faker.company.companyName(),
+    };
+  },
+  genMember () {
+    return {
+      memberKey: getRandomInt(),
+      memberFirstName: faker.name.firstName(),
+      memberLastName: faker.name.lastName(),
+      officeKey: getRandomKey(METADATA.OFFICE),
+    };
+  },
+  genProperty () {
+    return {
+      listingKey: getRandomInt(),
+      unparsedAddress: `${faker.address.streetAddress()}, ${faker.address.streetName()}`,
+      listPrice: faker.commerce.price(),
+      listAgentKey: getRandomKey(METADATA.MEMBER),
+      listOfficeKey: getRandomKey(METADATA.OFFICE),
+    };
+  },
+  genMedia () {
+    return {
+      mediaKey: getRandomInt(),
+      shortDescription: faker.lorem.words(),
+      mediaUrl: faker.internet.url(),
+      resourceName: METADATA.Property,
+      resourceRecordKey: getRandomKey(METADATA.PROPERTY),
+    };
+  },
+  genOpenHouse () {
+    return {
+      openHouseKey: getRandomInt(),
+      listingKey: getRandomKey(METADATA.PROPERTY),
+      openHouseDate: faker.date.future(),
+      showingAgentKey: getRandomKey(METADATA.MEMBER),
+      openHouseRemarks: faker.lorem.words(),
+    };
+  },
+  genEvents (numEvents=DEFAULT_POOL_SIZE) {
+    let data, eventData, total = 0, key, totalEvents = numEvents * Object.keys(METADATA).length;
+
+    
+    console.log(`\nGenerating ${totalEvents} Events!`);
+    _generatorProgressBar.start(totalEvents, 0, {speed: 1000});
+
+
+    Object.keys(METADATA).forEach(keyName => {
+      let {name, keyField, generator} = METADATA[keyName];
+
+      for (let i = 0; i < numEvents; i++) {
+        data = eval(generator)();
+        key = data[keyField];
+
+        setResourceData(keyName, key, data);
+        eventData = writeLogEvent(name, key);
+        LOG(`Event Written: eventSequence: ${total}, name: ${name}, keyField: ${keyField}, key: ${data[`${keyField}`]},
+          resourceRecordUrl: ${eventData.resourceRecordUrl}`);
+
+        total += 1;
+        _generatorProgressBar.increment();
+      } 
+    });
+   
+    _generatorProgressBar.stop();
+    if (_eventLog.length > total) {
+      console.log(`\nTotal log entries: ${_eventLog.length}\n`);
+    }
+  },
+};
+
 
 //holds the local generated cache of items in memory
 const _resourceCache = {
@@ -31,19 +109,15 @@ const _resourceCache = {
 
 //the event log is an array of events keyed by sequence number
 //where the sequence number is the array index.
-//event schema is: {resourceName, resourceKey}
+//event schema is: {resourceName, resourceKey, resourceRecordUrl}
 const _eventLog = [];
 
 //generates an event log and related resource data
 const initData = () => {
   console.log(`Event generation started. Pool size is ${DEFAULT_POOL_SIZE} events for each resource.`);
-  _generatorProgressBar.start(DEFAULT_POOL_SIZE * Object.keys(METADATA).length, 0);
   
-  randEvents(DEFAULT_POOL_SIZE);
+  RAND.genEvents(DEFAULT_POOL_SIZE);
   
-  _generatorProgressBar.stop();
-  console.log(`Event generation complete.`);
-
   console.log('\nSample data:');
   Object.keys(METADATA).forEach(key => {
     let {name, keyField} = METADATA[key], {keyCache} = _resourceCache[name];
@@ -57,8 +131,9 @@ const getEvents = (fromEventSequence=0) => _eventLog.slice(fromEventSequence);
 
 const writeLogEvent = (resourceName, resourceKey) => {
   let resourceRecordUrl = createRecordUrl(resourceName, resourceKey);
-  LOG(`resourceName: ${resourceName}, resourceKey: ${resourceKey}, resourceRecordUrl: ${resourceRecordUrl}`);
-  _eventLog.push({resourceName, resourceKey, resourceRecordUrl})
+  let data = {resourceName, resourceKey, resourceRecordUrl}
+  _eventLog.push(data);
+  return data;
 };
 
 const getResourceData = (resourceName, resourceId) => _resourceCache[resourceName][resourceId] || null;
@@ -67,7 +142,7 @@ const setResourceData = (keyName, resourceId, data={}) => {
       resource = _resourceCache[resourceName][resourceId];
 
   if (!resource) {
-    resource = eval(METADATA[keyName].generator)();
+    resource = eval(METADATA[keyName].generator)(); //ohhh so eval
     resource[METADATA.keyField] = resourceId;
     _resourceCache[resourceName].keyCache.push(resourceId);
   }
@@ -80,74 +155,11 @@ const getRandomKey = resourceMeta => {
   return getResourceData(name, keyCache[getRandomInt(keyCache.length)])[resourceMeta.keyField];
 };
 
-//TODO: use something like JSM, which adds faker to JSON Schema models.
-const randOffice = () => { 
-  return {
-    officeKey: getRandomInt(),
-    officeName: faker.company.companyName(),
-  };
-};
-
-const randMember = () => {
-  return {
-    memberKey: getRandomInt(),
-    memberFirstName: faker.name.firstName(),
-    memberLastName: faker.name.lastName(),
-    officeKey: getRandomKey(METADATA.OFFICE),
-  };
-};
-
-const randProperty = () => {
-  return {
-    listingKey: getRandomInt(),
-    unparsedAddress: `${faker.address.streetAddress()}, ${faker.address.streetName()}`,
-    listPrice: faker.commerce.price(),
-    listAgentKey: getRandomKey(METADATA.MEMBER),
-    listOfficeKey: getRandomKey(METADATA.OFFICE),
-  };
-};
-
-const randMedia = () => {
-  return {
-    mediaKey: getRandomInt(),
-    shortDescription: faker.lorem.words(),
-    mediaUrl: faker.internet.url(),
-    resourceName: METADATA.Property,
-    resourceRecordKey: getRandomKey(METADATA.PROPERTY),
-  };
-};
-
-const randOpenHouse = () => {
-  return {
-    openHouseKey: getRandomInt(),
-    listingKey: getRandomKey(METADATA.PROPERTY),
-    openHouseDate: faker.date.future(),
-    showingAgentKey: getRandomKey(METADATA.MEMBER),
-    openHouseRemarks: faker.lorem.words(),
-  };
-};
-
-const randEvents = (numEvents=1000) => {
-  let data, total = 0, key;
-
-  Object.keys(METADATA).forEach(keyName => {
-    let {name, keyField, generator} = METADATA[keyName];
-
-    for (let i = 0; i < numEvents; i++) {
-      total += 1;
-      data = eval(generator)(); //ohhh so eval
-      key = data[keyField];
-      LOG(`eventSequence: ${total}, name: ${name}, keyField: ${keyField}, key: ${data[`${keyField}`]}`);
-      setResourceData(keyName, key, data);
-      writeLogEvent(name, key);
-      _generatorProgressBar.increment();
-    } 
-  });
-};
 
 module.exports = {
   getEvent,
   getEvents,
   initData,
   getResourceData,
+  RAND
 }
